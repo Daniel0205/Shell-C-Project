faf0948246b552f0e3bda9caafbc5fa09bddbd75
Daniel0205 <daniel.alejandro.diaz@correounivalle.edu.co>
2018-05-11 17:58:57 -0500

    Shell con archivos
    Salida del comando dirigida a un archivo

--- shell.c
+++ shell.c
@@ -3,8 +3,9 @@
 #include <unistd.h>
 #include <string.h>
 #include <sys/wait.h>
+#include <fcntl.h>
 
-void asignarArgumentos(char ** arrayArgs, char *linea){
+void asignarArgumentos(char ** arrayArgs, char *linea,int *text){
 	char * aux;
 	int contador=0;
 	aux= strtok (linea," ");
@@ -13,11 +14,25 @@ void asignarArgumentos(char ** arrayArgs, char *linea){
 		arrayArgs[contador]=strdup(aux);
 		aux= strtok (NULL," ");
 		contador++;
-		
+		if(aux==NULL)break;
+		if (!strcmp(">", aux)){
+			*text=contador+2;
+			arrayArgs[contador]=NULL;
+			contador++;
+		}
 	}
 	arrayArgs[contador]=NULL;	
 }
 
+void prepararSalidaArchivo(int posicion,int* archivo,char ** args){
+	
+	if (posicion){
+		close(STDOUT_FILENO);
+		char *filename=args[posicion];
+		*archivo=open(filename,O_CREAT|O_WRONLY|O_TRUNC|S_IRWXU);		
+		
+	}
+}
 
 int main(int argc, char*argv[]) {
 
@@ -29,12 +44,29 @@ int main(int argc, char*argv[]) {
 			break;
 		}		
 		pid_t pid = fork();
+		int salidaTexto=0;
+		int fd;
 
 		if(!pid){
 			char * myargs[100];                
-			asignarArgumentos(myargs,comando); 
+			asignarArgumentos(myargs,comando,&salidaTexto);
+			prepararSalidaArchivo(salidaTexto,&fd,myargs);			
+				
+			/*if (salidaTexto){	
+				printf(". ");		
+				close(STDOUT_FILENO);
+				char *filename=myargs[salidaTexto];
+				fd=open(filename,O_CREAT|O_WRONLY|O_TRUNC|S_IRWXU|S_IXOTH);		
+			}*/
   			int aux = execvp(myargs[0],myargs); 
   			if (aux==-1)printf("Este comando no pudo ser ejecutado\n");
+  			if (salidaTexto) close(fd);
+  			if (fd<0){
+				printf("%d  error!",fd);
+				
+			}
+  			printf("%d  error!",fd);
+				
   			break;
   		}else{
   			wait(NULL);
